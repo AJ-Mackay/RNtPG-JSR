5ba27053f627e011c3a2ed6fc459319103342bbd
Paul Mackay <paul.m.mackay85@gmail.com>
2021-06-07 15:03:25 +0100

    Core Syntax Refresher

--- play.js
+++ play.js
@@ -1,3 +1,18 @@
-var name = 'Paul';
+var name = 'Paul'; //string
+var age = 36; //number
+var hasHobbies = true //boolean;
 
-console.log(name);
+// console.log(name);
+
+function summerizeUser(userName, userAge, userHasHobby) {
+    return (
+        'Name is ' +
+        userName +
+        ', age is ' +
+        userAge +
+        ' and the user has hobbies: ' +
+        userHasHobby
+    );
+}
+
+console.log(summerizeUser(name, age, hasHobbies));
