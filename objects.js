8027a9b2c372ec68982034e3cd7e66c15115af9a
Paul Mackay <paul.m.mackay85@gmail.com>
2021-06-07 15:24:55 +0100

    Arrays, Objects & Reference Types

--- objects.js
+++ objects.js
@@ -13,5 +13,8 @@ const hobbies = ['Reading', 'Puzzles'];
 // for (let hobby of hobbies) {
 //     console.log(hobby);
 // }
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+
+hobbies.push('Programming');
 console.log(hobbies);
